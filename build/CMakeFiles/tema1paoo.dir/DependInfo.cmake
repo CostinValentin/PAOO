df71024ba9889bad62b7c74a721adb021bc9bc81
CostinValentin <80770250+CostinValentin@users.noreply.github.com>
2022-12-01 16:06:40 +0200

    Build folder

---
+++ build/CMakeFiles/tema1paoo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/valentin/Desktop/PAOOTEMA1/tema1paoo.cpp" "/home/valentin/Desktop/PAOOTEMA1/build/CMakeFiles/tema1paoo.dir/tema1paoo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tema1paoo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema1paoo.dir/tema1paoo.cpp.o"
+  "tema1paoo"
+  "tema1paoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema1paoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
